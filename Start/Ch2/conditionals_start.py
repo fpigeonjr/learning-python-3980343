dc89f9ffd19ec3266bbb0580a3b1553a8989a242
Frank Pigeon Jr <fpigeon@flexion.us>
2025-05-02 09:37:16 -0500

    completes conditionals assigmenet

--- Start/Ch2/conditionals_start.py
+++ Start/Ch2/conditionals_start.py
@@ -1,9 +1,14 @@
-# LinkedIn Learning Python course by Joe Marini
-# Example file for working with conditional statements
-
-
-x, y = 10, 100
+x, y = 10, 100 # can declare multiple variables on one line
 
 # conditional flow uses if, elif, else
+if x < y:
+    print('x is less than y')
+elif x > y:
+    print('x is greater than y')
+else:
+    print('x is equal to y')
 
 # conditional statements let you use "a if C else b"
+# ternary is js
+result = "x is less than y" if (x < y) else "x is geater or equal to y"
+print(result)
