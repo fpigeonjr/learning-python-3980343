c0ea25813a3f8e94ef7d1e5a54eec963369232a9
Frank Pigeon Jr <fpigeon@flexion.us>
2025-05-03 13:08:14 -0500

    completes modules lesson

--- Start/Ch3/modules_start.py
+++ Start/Ch3/modules_start.py
@@ -1,19 +1,19 @@
-# LinkedIn Learning Python course by Joe Marini
-# Working with modules of code
-
 # import the math module, which contains features for working with mathematics
+import math
 
-
+print(f'the square roor of 16 is {math.sqrt(16)}')
 # import a specific part of the module so you can refer to it more easily
-
+from math import pi
+print(f'Pi is: {pi}')
 
 # import a module and give it a different name
+import random as r
 
-
+print(r.randint(100, 200))
 # the math module contains lots of pre-built functions
 
 
-# in addition to functions, some modules contain useful constants 
+# in addition to functions, some modules contain useful constants
 
 
 # Generate a random number between 100 and 200
@@ -22,6 +22,8 @@
 # try some of the math functions for yourself here:
 
 # Use the 3rd party tabulate module to print tabulated data:
+
+from tabulate import tabulate
 
 # Sample data
 data = [
@@ -32,3 +34,4 @@ data = [
 ]
 
 # Create a formatted table
+print(tabulate(data, headers="firstrow", tablefmt="github"))
