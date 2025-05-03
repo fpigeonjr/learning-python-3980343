54446d99e9ed4ae6a1618c17142cc0742add6e1d
Frank Pigeon Jr <fpigeon@flexion.us>
2025-05-02 20:12:33 -0500

    completes challenge

--- Start/Ch2/challenge.py
+++ Start/Ch2/challenge.py
@@ -23,3 +23,30 @@ def count_number(which, numbers):
 print(count_number('even', numbers)) # 3
 print(count_number('odd', numbers)) # 2
 print(count_number('bla', numbers)) # -1
+
+
+# better solution
+#
+def isEven(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+def count_number2(which, numbers):
+    which = which.lower()
+    count = 0
+
+    if which != 'odd' and  which != 'even':
+        return -1
+
+    for num in numbers:
+        if which == "even" and isEven(num):
+            count += 1
+        elif which == "odd" and not isEven(num):
+            count += 1
+    return count
+
+print(count_number2('even', numbers)) # 3
+print(count_number2('odd', numbers)) # 2
+print(count_number2('bla', numbers)) # -1
