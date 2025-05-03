ea53f4347d206f747ef3b137441958d4d8e7b195
Frank Pigeon Jr <fpigeon@flexion.us>
2025-05-03 13:40:40 -0500

    completes excemptions lessons

--- Start/Ch3/exceptions_start.py
+++ Start/Ch3/exceptions_start.py
@@ -1,13 +1,22 @@
-# LinkedIn Learning Python course by Joe Marini
-# Example file for working with Exceptions
-#
-
 # Errors can happen in programs, and we need a clean way to handle them
 # This code will cause an error because you can't divide by zero:
-
-# Exceptions provide a way of catching errors and then handling them in 
+# x = 10 / 0
+# Exceptions provide a way of catching errors and then handling them in
 # a separate section of the code to group them together
-
+try:
+    x = 10 / 0
+except:
+    print('error')
 
 # You can also catch specific exceptions
-
+try:
+    answer = input("What should I divide 10 by: ")
+    num = int(answer)
+    print(f"10 divided by {num} is {10 / num}")
+except ZeroDivisionError as e:
+    print("You can't divide by zero")
+except ValueError as e:
+    print("You didn't provide a valid number")
+    print(e)
+finally:
+    print("Finally always runs")
