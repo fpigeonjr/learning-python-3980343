76bd1ca9f457a437b92a71a67f906852f42fb728
Frank Pigeon Jr <fpigeon@flexion.us>
2025-05-03 11:48:44 -0500

    complete lesson on built in Python functions

--- Start/Ch3/builtin_start.py
+++ Start/Ch3/builtin_start.py
@@ -1,25 +1,32 @@
-# LinkedIn Learning Python course by Joe Marini
-# Example file for using built-in functions
-#
-
 mystring = "The quick, brown fox jumped over the lazy dog!"
 mynumbers = [1,3,5,6,9,12,14,17,20,30]
 
 # the len() function calculates the length of a sequence
-
+print(len(mystring))
+print(len(mynumbers))
 
 # the max() and min() functions will find the largest and smallest value in a sequence
+print(min(mynumbers))
+print(max(mynumbers))
 
 
 # the str() function will return a string version of an object
 prefix = "result: "
 result = 5
 
+print(prefix + str(result))
 
-# range(start, stop, step) will create a range of numbers 
-# You can use ranges along with loops 
 
+# range(start, stop, step) will create a range of numbers
+# You can use ranges along with loops
+for i in range(0, 110, 10):
+    print(i)
+
+for i in range(5, len(mystring), 2):
+    print(mystring[i])
 
 # the print function itself is pretty flexible - you can embed variables directly in it
 greeting = "Hello!"
 count = 10
+
+print(f"{greeting} you are visitor number {count}")
