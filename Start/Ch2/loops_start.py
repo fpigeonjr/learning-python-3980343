9db67c547a13b340f69dbad7f00a1e5c21c47339
Frank Pigeon Jr <fpigeon@flexion.us>
2025-05-02 19:32:56 -0500

    completes loops exercise

--- Start/Ch2/loops_start.py
+++ Start/Ch2/loops_start.py
@@ -1,19 +1,30 @@
-# LinkedIn Learning Python course by Joe Marini
-# Example file for working with loops
-
-
 x = 0
 
 # define a while loop
+while x < 5:
+    print(x)
+    x= x + 1
 
+# another example
+answer = input("Should I stop?")
+while answer != "yes":
+    print(answer)
+    answer = input("Should I stop?")
 
 # define a for loop
-
-
-# use a for loop over a collection
-
+days = ['M', 'T', 'W', 'Th', 'F']
+for d in days:
+    print(d)
 
 # use the break and continue statements
-
+days = ['M', 'T', 'W', 'Th', 'F']
+for d in days:
+    if(d == "W"):
+        break # terminates loop
+    if(d == "Th"):
+        continue # skips this iteration
+    print(d)
 
 # using the enumerate() function to get an index and an item
+for i, d in enumerate(days):
+    print(i, d) # 0 M, 1 T, etc
