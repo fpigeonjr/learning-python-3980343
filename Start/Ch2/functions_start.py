0ccd59e3517d7f422fbf7d965cb913b3ce13349b
Frank Pigeon Jr <fpigeon@flexion.us>
2025-05-02 19:32:44 -0500

    completes functions exercise

--- Start/Ch2/functions_start.py
+++ Start/Ch2/functions_start.py
@@ -1,19 +1,37 @@
-# LinkedIn Learning Python course by Joe Marini
-# Example file for working with functions
-
-
 # define a basic function
-print("hello world!")
-name = input("What is your name? ")
-print("Nice to meet you,", name)
+def hello():
+    print("hello world!")
+    name = input("What is your name? ")
+    print("Nice to meet you,", name)
+
+hello()
 
 # function that takes parameters
+def helloName(greeting):
+    name = input("What is your name? ")
+    print(greeting, name)
 
+helloName('Was up')
 
 # function that returns a value
+def cube(x):
+    return x * x * x
 
+print(cube(3))
 
 # function with default value for an parameter
+def helloName(greeting = 'Nice to meet you', name = None):
+    if (name == None):
+        name = input("What is your name? ")
+    print(greeting, name)
 
+helloName("Was happening")
+helloName(name="Frank", greeting="Was up") # allows you to change the order
 
 # function with variable number of parameters
+def multi_add(start, *args):
+    result = start
+    for x in args:
+        result += x
+    return result
+print(multi_add(100, 2, 3, 4))
